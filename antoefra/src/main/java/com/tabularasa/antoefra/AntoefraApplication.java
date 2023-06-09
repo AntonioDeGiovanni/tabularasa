b03a2b886ca625c0998eb60838283fd7522947ce
De Giovanni <antonio.de.giovanni@accenture.com>
2023-06-09 12:32:17 +0200

    first commit

---
+++ antoefra/src/main/java/com/tabularasa/antoefra/AntoefraApplication.java
@@ -0,0 +1,13 @@
+package com.tabularasa.antoefra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntoefraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntoefraApplication.class, args);
+	}
+
+}
---
+++ antoefra/src/test/java/com/tabularasa/antoefra/AntoefraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tabularasa.antoefra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntoefraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
